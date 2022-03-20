3842de86184c0e4d5d5d3f4dd9c402c5027270a8
yessense <Akorchemnyj@gmail.com>
2022-03-20 15:18:22 +0300

    Added inference dataset

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -27,7 +27,7 @@ class MultiDisDsprites(IterableDataset):
         self.features_count = [3, 6, 40, 32, 32]
         self.features_range = [list(range(i)) for i in self.features_count]
         self.multiplier = list(itertools.accumulate(self.features_count[-1:0:-1], operator.mul))[::-1] + [1]
-        print(self.multiplier)
+        # print(self.multiplier)
 
     def get_element_pos(self, labels: np.ndarray) -> int:
         pos = 0
@@ -153,67 +153,51 @@ if __name__ == '__main__':
     # dataset
     mdd = MultiDisDsprites(path='/home/yessense/PycharmProjects/multi-dis-dsprites/src/dataset/data/dsprite_train.npz')
 
-    # visual check if pair changes only one feature
-    # sample_size = 5
-    # for j in range(5):
-    #     fig, ax = plt.subplots(sample_size, 2)
-    #     for i in range(sample_size):
-    #         img, pair, exchange_labels = mdd.get_pair()
-    #         ax[i, 0].imshow(img.squeeze(0), cmap='gray')
-    #         ax[i, 1].imshow(pair.squeeze(0), cmap='gray')
-    #
-    #     plt.show()
 
-    # visual check of inference
+    def show_pairs(mdd: MultiDisDsprites, sample_size: int = 5):
+        fig, ax = plt.subplots(sample_size, 2)
+        for i in range(sample_size):
+            img, pair, exchange_labels = mdd.get_pair()
+            ax[i, 0].imshow(img.squeeze(0), cmap='gray')
+            ax[i, 1].imshow(pair.squeeze(0), cmap='gray')
 
-    sample_size = 5
-    fig, ax = plt.subplots(sample_size, 4)
-    for i in range(sample_size):
-        scene, image1, donor, image2 = mdd.inference_sample()
-        ax[i, 0].imshow(scene.detach().cpu().numpy().squeeze(0), cmap='gray')
-        ax[i, 1].imshow(image1.detach().cpu().numpy().squeeze(0), cmap='gray')
-        ax[i, 2].imshow(donor.detach().cpu().numpy().squeeze(0), cmap='gray')
-        ax[i, 3].imshow(image2.detach().cpu().numpy().squeeze(0), cmap='gray')
+        plt.show()
 
-    for i in range(sample_size):
-        for j in range(4):
-            ax[i, j].set_axis_off()
 
-    plt.show()
+    def show_inference_dataset(mdd: MultiDisDsprites, sample_size: int = 5):
+        fig, ax = plt.subplots(sample_size, 4)
+        for i in range(sample_size):
+            scene, image1, donor, image2 = mdd.inference_sample()
+            ax[i, 0].imshow(scene.detach().cpu().numpy().squeeze(0), cmap='gray')
+            ax[i, 1].imshow(image1.detach().cpu().numpy().squeeze(0), cmap='gray')
+            ax[i, 2].imshow(donor.detach().cpu().numpy().squeeze(0), cmap='gray')
+            ax[i, 3].imshow(image2.detach().cpu().numpy().squeeze(0), cmap='gray')
 
-    # batch_size = 5
-    # loader = DataLoader(mdd, batch_size=batch_size)
-    #
-    # for i, batch in enumerate(loader):
-    #     scenes1, scenes2, fist_objs, pair_objs, second_objs, exchange_labels = batch
-    #     if i % 1000 == 0:
-    #
-    #         fig, ax = plt.subplots(batch_size, 5, figsize=(5, 5))
-    #         for i in range(batch_size):
-    #             for j, column in enumerate(batch[:-1]):
-    #                 ax[i, j].imshow(column[i].detach().cpu().numpy().squeeze(0), cmap='gray')
-    #                 ax[i, j].set_axis_off()
-    #
-    #         plt.show()
-    #
-    #     assert torch.all(scenes1 == fist_objs + second_objs)
-    #     assert torch.all(scenes2 == pair_objs + second_objs)
-    #     break
-    #
-    # for i, batch in enumerate(loader):
-    #     scenes1, scenes2, fist_objs, pair_objs, second_objs, exchange_labels = batch
-    #     if i % 1000 == 0:
-    #
-    #         fig, ax = plt.subplots(batch_size, 5, figsize=(5, 5))
-    #         for i in range(batch_size):
-    #             for j, column in enumerate(batch[:-1]):
-    #                 ax[i, j].imshow(column[i].detach().cpu().numpy().squeeze(0), cmap='gray')
-    #                 ax[i, j].set_axis_off()
-    #
-    #         plt.show()
-    #
-    #     assert torch.all(scenes1 == fist_objs + second_objs)
-    #     assert torch.all(scenes2 == pair_objs + second_objs)
-    #     break
-    #
-    print("Done")
+        for i in range(sample_size):
+            for j in range(4):
+                ax[i, j].set_axis_off()
+
+        plt.show()
+
+
+    def show_training_dataset(mdd: MultiDisDsprites, batch_size: int = 5):
+        batch_size = 5
+        loader = DataLoader(mdd, batch_size=batch_size)
+
+        for i, batch in enumerate(loader):
+            scenes1, scenes2, fist_objs, pair_objs, second_objs, exchange_labels = batch
+            if i % 4000 == 0:
+
+                fig, ax = plt.subplots(batch_size, 5, figsize=(5, 5))
+                for i in range(batch_size):
+                    for j, column in enumerate(batch[:-1]):
+                        ax[i, j].imshow(column[i].detach().cpu().numpy().squeeze(0), cmap='gray')
+                        ax[i, j].set_axis_off()
+
+                plt.show()
+
+            assert torch.all(scenes1 == fist_objs + second_objs)
+            assert torch.all(scenes2 == pair_objs + second_objs)
+
+
+    show_inference_dataset(mdd, 5)
